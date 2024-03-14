f7b9d96061d02044916b58deab0be5b666ba0619
bholmesdev <bholmesdev@gmail.com>
2024-03-14 14:15:45 -0400

    deps: db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from "astro/config";
-
 import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [tailwind()],
+  integrations: [tailwind(), db()],
   vite: {
     esbuild: {
-      keepNames: true,
-    },
-  },
+      keepNames: true
+    }
+  }
 });
