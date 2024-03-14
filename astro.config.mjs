83567995d8143be0423da73160c9578a1d4d3e8b
bholmesdev <bholmesdev@gmail.com>
2024-03-14 17:06:37 -0400

    deps: astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,20 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
 import simpleStackForm from "simple-stack-form";
-
 import node from "@astrojs/node";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: node({
-    mode: "standalone",
+    mode: "standalone"
   }),
-  integrations: [tailwind(), db(), simpleStackForm()],
+  integrations: [tailwind(), db(), simpleStackForm(), icon()],
   vite: {
     esbuild: {
-      keepNames: true,
-    },
-  },
+      keepNames: true
+    }
+  }
 });
