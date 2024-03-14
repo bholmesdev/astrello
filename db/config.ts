be6465bcfe6625a391042ad91658f7adccadbd07
bholmesdev <bholmesdev@gmail.com>
2024-03-14 15:34:50 -0400

    feat(db): initial config

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,30 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable } from "astro:db";
+
+const Board = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    slug: column.text(),
+    name: column.text(),
+  },
+});
+
+export const COLUMN = {
+  TODO: "todo",
+  DONE: "done",
+  IN_PROGRESS: "in_progress",
+};
+
+const Card = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    boardId: column.number({ references: () => Board.columns.id }),
+    name: column.text(),
+    description: column.text({ optional: true }),
+    column: column.text({ default: COLUMN.TODO }),
+  },
+});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: { Board, Card },
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,11 @@
-import { db } from 'astro:db';
+import { Board, db } from "astro:db";
+import Slugger from "github-slugger";
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+  const slugger = new Slugger();
+  await db.insert(Board).values({
+    name: "The Ben Board",
+    slug: slugger.slug("The Ben Board"),
+  });
 }
