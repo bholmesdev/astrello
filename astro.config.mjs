1955af9b49d5b99f941f48a01fde924bf1e1540f
bholmesdev <bholmesdev@gmail.com>
2024-03-14 15:34:30 -0400

    deps: switch to node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
-
 import db from "@astrojs/db";
+import simpleStackForm from "simple-stack-form";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare(),
-  integrations: [tailwind(), db()],
+  adapter: node({
+    mode: "standalone",
+  }),
+  integrations: [tailwind(), db(), simpleStackForm()],
   vite: {
     esbuild: {
-      keepNames: true
-    }
-  }
+      keepNames: true,
+    },
+  },
 });
