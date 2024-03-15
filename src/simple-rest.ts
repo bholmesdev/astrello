f8071455f551707a1ae4b87a15b64d1406f4d85a
bholmesdev <bholmesdev@gmail.com>
2024-03-15 07:40:52 -0400

    feat: parseFetch utility

---
+++ src/simple-rest.ts
@@ -0,0 +1,63 @@
+type Method = "GET" | "POST" | "PUT" | "DELETE" | "PATCH" | "OPTIONS" | "HEAD";
+
+const DOMMimeType = [
+  "text/html",
+  "text/xml",
+  "application/xml",
+  "application/xhtml+xml",
+  "image/svg+xml",
+] as const;
+
+type DOMMimeType = (typeof DOMMimeType)[number];
+
+const MimeType = [
+  "text/plain",
+  "application/json",
+  "multipart/form-data",
+  "application/x-www-form-urlencoded",
+  ...DOMMimeType,
+] as const;
+
+type MimeType = (typeof MimeType)[number];
+
+function isDomMimeType(mimeType: string): mimeType is DOMMimeType {
+  return DOMMimeType.includes(mimeType as any);
+}
+
+export async function parseFetch<
+  TAccept extends MimeType | undefined = undefined
+>(
+  url: string | URL,
+  init: RequestInit & {
+    headers?: {
+      "Content-Type"?: MimeType;
+      Accept?: TAccept;
+      [key: string]: string | undefined;
+    };
+    method?: Method;
+  }
+): Promise<
+  Omit<Response, "body"> & {
+    body: TAccept extends DOMMimeType
+      ? Document
+      : TAccept extends "application/json"
+      ? any
+      : string;
+  }
+> {
+  const { Accept } = init.headers || {};
+  const response = await fetch(url, init);
+  if (Accept === "application/json") {
+    Object.assign(response, { body: await response.json() });
+    return response as any;
+  }
+  if (Accept && isDomMimeType(Accept)) {
+    const domParser = new DOMParser();
+    Object.assign(response, {
+      body: domParser.parseFromString(await response.text(), Accept),
+    });
+    return response as any;
+  }
+  Object.assign(response, { body: await response.text() });
+  return response as any;
+}
